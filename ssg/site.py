7486b049f316c5fa05dbec23a76aa94945f6529d
roshan-pnq <rpzhank1@gmail.com>
2021-10-10 11:13:29 +0530

    Making syntax corrections

--- ssg/site.py
+++ ssg/site.py
@@ -1,21 +1,23 @@
 from os import mkdir
 from pathlib import Path
+
+
 class Site:
 
-    def __init__(self,source,dest) -> None:
+    def __init__(self, source, dest) -> None:
         self.source = Path(source)
         self.dest = Path(dest)
 
-    def create_dir(self,path):
+    def create_dir(self, path):
         directory = self.dest/path.relative_to(self.source)
+        #debugging statement
+        print(directory)
 
-        directory.mkdir(parents = True,exists_ok = True)
+        directory.mkdir(parents=True, exists_ok=True)
+        print(directory)
 
     def build(self):
-        self.dest.mkdir(parents=True,exist_ok=True)
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-
-    
-        
